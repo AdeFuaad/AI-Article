b5cfed4b74830da5c569cf8a052568cceed6e3f1
AdeFuaad <adesholafuaad@gmail.com>
2023-05-27 13:15:27 -0400

    AI sum

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import { ade } from '/src/assets/index.js';
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={ade} alt="logo" className="w-14 object-contain" />
+
+        <button type='button' onClick={() => window.open('https://github.com/adefuaad')} className="black_btn">change to resume</button>
+
+        {/* <button type='button' onClick={() => window.open('https://link-to-your-resume', '_blank')} className="black_btn">Resume</button> */}
+
+      </nav>
+
+      <h1 className="head_text">
+        Artificial Intelligence for <br className='max-md:hidden'/>
+        <span className='orange_gradient'>Website Synopsis Generation</span>
+      </h1>
+
+      <h2 className='desc mb-10'>
+        Revolutionizing Summaries: AI-Powered Article Condensation
+      </h2>
+    </header>
+  )
+}
+
+export default Hero
